b97eb6e633fb13bf0a9c84fc04304e0318ba8a04
Richard Downer <richard.downer@cloudsoftcorp.com>
2013-07-16 16:33:45 +0100

    squash

--- google-compute-engine/src/test/java/org/jclouds/googlecomputeengine/features/ImageApiExpectTest.java
+++ google-compute-engine/src/test/java/org/jclouds/googlecomputeengine/features/ImageApiExpectTest.java
@@ -39,7 +39,7 @@ import static org.testng.AssertJUnit.assertNull;
 @Test(groups = "unit")
 public class ImageApiExpectTest extends BaseGoogleComputeEngineApiExpectTest {
 
-   static final String MYPROJECT_IMAGES_RESOURCE_URL = MYPROJECT_BASE_URL + "/images";
+   public static final String MYPROJECT_IMAGES_RESOURCE_URL = MYPROJECT_BASE_URL + "/images";
    static final String GOOGLE_IMAGES_RESOURCE_URL = GOOGLE_PROJECT_BASE_URL + "/images";
 
    public static final HttpRequest LIST_PROJECT_IMAGES_REQUEST = HttpRequest
